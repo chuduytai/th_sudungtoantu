ac5f6719f3109cf6f427f31379bf8a6cb286d180
= <=>
2020-01-06 14:54:46 +0700

    add

---
+++ nhungtoantu.js
@@ -0,0 +1,17 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt("Nhap chieu rong");
+inputHeight = prompt("Nhap chieu cao");
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width*height;
+document.write("dien tich la: "+ area);
+
+
+let a = parseInt(prompt('nhap vao chieu rong'));
+let b = parseInt(prompt('nhap vao chieu cao'));
+let dientich;
+dientich=a*b;
+document.write('dien tich la: ' + dientich);
